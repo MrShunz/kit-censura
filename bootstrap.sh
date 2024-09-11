d1d931ef79e2ffbe07dd75fe3ba7aae88831df44
Daniele Santi <daniele@santi.vr.it>
2024-09-11 15:01:51 +0200

    Small code cleanups

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,5 @@
 #!/bin/bash -x 
 
-#echo "Downloading external tools..."
-
-# use local copy
-#wget "https://github.com/mphilosopher/censura/raw/master/src/download_agcom.py" -O download_agcom.py
-#chmod +x download_agcom.py
-#
-#wget "https://github.com/mphilosopher/censura/raw/master/src/download_consob.py" -O download_consob.py
-#chmod +x download_consob.py
-
 echo "Creating new empty files..."
 
 touch lista.manuale
