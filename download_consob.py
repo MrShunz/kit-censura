e73dd0b8d5c140b6515b58e27bb5139a324b1076
Daniele Santi <daniele@santi.vr.it>
2024-11-27 14:48:27 +0100

    oops: Correctly build TLDextract passing       updated url

--- download_consob.py
+++ download_consob.py
@@ -3,7 +3,7 @@
 import urllib3
 import requests, optparse, re
 from urlextract import URLExtract
-from tldextract import extract
+from tldextract import TLDExtract
 from collections import OrderedDict
 from bs4 import BeautifulSoup
 from bs4.element import Tag
@@ -12,17 +12,23 @@ from random import randint
 from time import sleep
 import ua_generator
 
+
 # Funzione di Attesa Randomica per evitare di essere identificati come bot di Scraping
 def waitRand():
-    waitms = randint(500,2000)/1000
+    waitms = randint(500, 2000) / 1000
     sleep(waitms)
+
 
 def main():
     global options
 
     # Imposta le variabili per il WAF
     referer = "https://www.consob.it/web/area-pubblica/occhio-alle-truffe"
-    user_agent = ua_generator.generate(device='desktop', platform=('windows', 'macos', 'linux'), browser=('chrome', 'edge', 'firefox', 'safari')).text
+    user_agent = ua_generator.generate(
+        device="desktop",
+        platform=("windows", "macos", "linux"),
+        browser=("chrome", "edge", "firefox", "safari"),
+    ).text
     accept_language = "it-IT,it;"
     accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"
     cookies = None
@@ -38,55 +44,76 @@ def main():
     (options, args) = parser.parse_args()
     if len(args) == 1:
         parser.error("Numero di argomenti non corretto")
-    if (options.out_file is None):
+    if options.out_file is None:
         parser.error("Numero di argomenti non corretto")
 
-    curpage=1
-    totpages=0
+    curpage = 1
+    totpages = 0
     urls = []
-    while(curpage<=totpages or totpages==0):
+    extract = TLDExtract(
+        suffix_list_url="https://raw.github.com/mozilla/gecko-dev/master/netwerk/dns/effective_tld_names.dat",
+        cache_file="/tmp/tldextract",
+    )
+    while curpage <= totpages or totpages == 0:
         try:
-            url = "https://www.consob.it/web/area-pubblica/oscuramenti?p_p_id=com_liferay_asset_publisher_web_portlet_AssetPublisherPortlet_INSTANCE_m9PTOY4SM1GU&_com_liferay_asset_publisher_web_portlet_AssetPublisherPortlet_INSTANCE_m9PTOY4SM1GU_cur={}".format(curpage)
-            if(curpage > 1): waitRand()
+            url = "https://www.consob.it/web/area-pubblica/oscuramenti?p_p_id=com_liferay_asset_publisher_web_portlet_AssetPublisherPortlet_INSTANCE_m9PTOY4SM1GU&_com_liferay_asset_publisher_web_portlet_AssetPublisherPortlet_INSTANCE_m9PTOY4SM1GU_cur={}".format(
+                curpage
+            )
+            if curpage > 1:
+                waitRand()
             s = requests.Session()
-            s.headers.update({'Referer': referer, 'User-Agent': user_agent, 'Accept-Language': accept_language, 'Accept': accept, 'Upgrade-Insecure-Requests': '1' })
+            s.headers.update(
+                {
+                    "Referer": referer,
+                    "User-Agent": user_agent,
+                    "Accept-Language": accept_language,
+                    "Accept": accept,
+                    "Upgrade-Insecure-Requests": "1",
+                }
+            )
             page = s.get(url, cookies=cookies, verify=False)
             cookies = page.cookies
             referer = url
-            if((str(page.content)).find("you are a bot")!=-1):
+            if (str(page.content)).find("you are a bot") != -1:
                 print("WAF detected BOT, sorry!")
-                return(False)
+                return False
             soup = BeautifulSoup(page.content, "html.parser")
             # Recupera il numero di pagine totali se non recuperato ancora
-            if(totpages==0):
-                for span in soup.findAll('span', attrs={'class':'lfr-icon-menu-text'}):
-                    pag=span.get_text()
-                    totpages=int(re.search(r"[0-9]{1,2}$",pag).group(0))
+            if totpages == 0:
+                for span in soup.findAll("span", attrs={"class": "lfr-icon-menu-text"}):
+                    pag = span.get_text()
+                    totpages = int(re.search(r"[0-9]{1,2}$", pag).group(0))
                     # Esce se non ha trovato il numero di pagine
-                    if(totpages==0):
+                    if totpages == 0:
                         print("Non posso determinare il numero di pagine della lista")
-                        return(False)
+                        return False
             # Ricerca il contenuto
-            for div in soup.find_all('div', attrs={'class':'divContent'}):
-                for comunicato in div.find_all('h4'):
-                    block = comunicato.find_next('div')
-                    if(not block):
-                       continue
+            for div in soup.find_all("div", attrs={"class": "divContent"}):
+                for comunicato in div.find_all("h4"):
+                    block = comunicato.find_next("div")
+                    if not block:
+                        continue
                     content = str(block.get_text().encode("ascii", "ignore"))
                     extractor = URLExtract()
                     for url in extractor.find_urls(content):
                         ext = extract(url)
-                        if ext.domain == "consob" and ext.suffix == "it": continue
-                        if ext.domain == "europa" and ext.suffix == "eu": continue
-                        url = (ext.subdomain+"."+ext.domain+"."+ext.suffix) if ext.subdomain != "" else (ext.domain+"."+ext.suffix)
+                        if ext.domain == "consob" and ext.suffix == "it":
+                            continue
+                        if ext.domain == "europa" and ext.suffix == "eu":
+                            continue
+                        url = (
+                            (ext.subdomain + "." + ext.domain + "." + ext.suffix)
+                            if ext.subdomain != ""
+                            else (ext.domain + "." + ext.suffix)
+                        )
                         urls.append(url)
             curpage = curpage + 1
         except Exception:
             continue
     # Scrive la lista
-    with open(options.out_file,'wb') as f:
+    with open(options.out_file, "wb") as f:
         f.write("\n".join(urls).encode())
 
-if __name__ == '__main__':
-    main()
 
+if __name__ == "__main__":
+    main()
